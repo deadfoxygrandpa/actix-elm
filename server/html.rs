20389718909d5ca490b70947bae4b305e21cd1b4
deadfoxygrandpa <root@Nemi.localdomain>
2020-02-03 20:39:43 +0800

    use google webfont loader for now

--- server/html.rs
+++ server/html.rs
@@ -15,8 +15,7 @@ where T: Serialize
 <meta charset=\"utf-8\">\
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
 <title>App</title>\
-<link href=\"/fonts/noto-sans-sc-v10-latin_chinese-simplified-900.woff2\"rel=\"preload\">\
-<link href=\"/fonts/noto-sans-sc-v10-latin_chinese-simplified-regular.woff2\"rel=\"preload\">\
+<link href=\"https://fonts.googleapis.com/css?family=Noto+Sans+SC:400,900&display=swap&subset=chinese-simplified\"rel=\"stylesheet\">\
 <link href=\"/style.css\" rel=\"stylesheet\">\
 <script src=\"/elm.js\"></script>\
 </head>\
